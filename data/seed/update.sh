efdd16fed0f9a2eb73fc479c219fb7d91e82dd92
lidingpku <lidingpku@69cf2eaa-3082-11df-9ac8-ff20597b2c9b>
2010-03-24 08:12:39 +0000

    server update

--- data/seed/update.sh
+++ data/seed/update.sh
@@ -1,4 +1,5 @@
 sort -iu job_ontology_todo.csv job_ontology.csv > temp;
-rm job_ontology.csv
+path="`date '+../log/%Y/%Y-%m-%d-log-job_ontology.csv'`"
+cp job_ontology.csv $path
 rm job_ontology_todo.csv
 mv temp job_ontology.csv
