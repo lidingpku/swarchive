b68e340db1247de207aecc78796c813e9a5dc380
lidingpku <lidingpku@69cf2eaa-3082-11df-9ac8-ff20597b2c9b>
2010-07-25 21:31:18 +0000

    server update

--- trunk/config/update.sh
+++ trunk/config/update.sh
@@ -1,5 +1,15 @@
-sort -iu job_ontology_todo.csv job_ontology.csv > temp;
-path="`date '+../log/%Y/%Y-%m-%d-log-job_ontology.csv'`"
-cp job_ontology.csv $path
-rm job_ontology_todo.csv
-mv temp job_ontology.csv
+path_ls="`date '+../data/log/%Y/'`"
+path_today_disc_o="`date '+../data/log/%Y/%Y-%m-%d-discover-ontology.csv'`"
+path_today_job="`date '+../data/log/%Y/%Y-%m-%d-archive-job.csv'`"
+path_tomorrow_job="`date --date=tomorrow '+../data/log/%Y/%Y-%m-%d-archive-job.csv'`"
+#echo $path_today_disc_o
+#echo $path_today_job
+#echo $path_tomorrow_job
+cat $path_today_disc_o $path_today_job > temp_job
+#sed -i '/^\#/d' temp_job
+sed -i 's/\#.*//' temp_job
+sed -i '/^file/d' temp_job
+sed -i '/^$/d' temp_job
+sort -iu temp_job > $path_tomorrow_job
+rm temp_job
+ls -lr $path_ls
