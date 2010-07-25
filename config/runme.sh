8f93cc0e807fd4e5b91f391c7eeb96cc705f2607
lidingpku <lidingpku@69cf2eaa-3082-11df-9ac8-ff20597b2c9b>
2010-07-25 21:31:18 +0000

    server update

--- config/runme.sh
+++ config/runme.sh
@@ -1,4 +1,11 @@
 cd ..
 cd java
-ant archive
-ant discover
+path_log_archive="`date '+../temp/log-%Y-%m-%d-archive'`"
+echo run $path_log_archive
+ant archive -l $path_log_archive
+path_log_discover="`date '+../temp/log-%Y-%m-%d-discover'`"
+echo run $path_log_discover
+ant discover -l $path_log_discover
+cd ..
+cd config
+./update.sh
