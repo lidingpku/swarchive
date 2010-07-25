b4f083fe618d30e472bc7e3469429bbbdb56283b
lidingpku <lidingpku@69cf2eaa-3082-11df-9ac8-ff20597b2c9b>
2010-07-25 21:37:16 +0000

    server update

--- config/init.sh
+++ config/init.sh
@@ -1,2 +1,6 @@
+path1="`date '+../data/log/'`"
+mkdir $path1
+path2="`date '+../data/log/%Y/'`"
+mkdir $path2
 path_job="`date '+../data/log/%Y/%Y-%m-%d-archive-job.csv'`"
 cp seed/job_ontology.csv $path_job
